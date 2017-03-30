3aa2618e9cb0f548971865827a9cb2c72b3def87
Chrissie <schieberchristine@gmail.com>
2017-03-30 22:05:09 +1100

    added functions, removed endpoint

--- CV.js
+++ CV.js
@@ -54,64 +54,71 @@ requestAnimationFrame(function() {
       $(this).toggleClass('show-description');
   });
 
-  // for (var i = 0 ; i < paragraphs.length; i++) {
-  //   paragraphs[i].addEventListener('click' , function() {
-  //     console.log("hi");
-  //     paragraphs[i].classList.toggle('show-description');
-  //   });
-  // };
+  // --- animate text bubble ---
+  function getElementCentre(element) {
+    var rect = element.getBoundingClientRect();
+    return {
+      x: rect.left + 0.5*rect.width,
+      y: rect.top  + 0.5*rect.height
+    }
+  }
+
+  function animateBubble(startingPosition, finalPosition, container, classname) {
+    var bubble = document.createElement("div");
+    bubble.setAttribute("class", classname);
+    bubble.style.left = startingPosition.x + 'px';
+    bubble.style.top  = startingPosition.y + 'px';
+    container.appendChild(bubble);
+
+    var finalShift = {
+      x: finalPosition.x - startingPosition.x,
+      y: finalPosition.y - startingPosition.y
+    };
+
+    var values = [
+      { x: 0, y: 0 },
+      finalShift
+    ];
+
+    TweenMax.to(bubble, timeScale * 0.3, {
+      bezier: {values: values, type: "soft"},
+      onComplete: function() {
+        bubble.remove();
+      }
+    });
+  }
 
   // --- paragraph stuff ---
   scrollable.addEventListener('scroll', function(e) {
 
     var midPoint = e.target.scrollTop + 0.45 * e.target.offsetHeight;
+    var bigBubble1 = document.querySelector('#group1 .bigbubble');
+    var bigBubble2 = document.querySelector('#group2 .bigbubble');
+    var bigBubble3 = document.querySelector('#group3 .bigbubble');
+    var finalPosition = {
+      '1': getElementCentre(bigBubble1),
+      '2': getElementCentre(bigBubble2),
+      '3': getElementCentre(bigBubble3)
+    };
 
     for (var i = 0; i < paragraphs.length; i++) {
       if (paragraphs[i].offsetTop < midPoint) {
         if (text_bubbles[i].getAttribute('class') === 'text-bubble') {
-          var position = text_bubbles[i].getBoundingClientRect();
+          var startingPosition = {
+            x: getElementCentre(text_bubbles[i]).x,
+            y: document.getElementById('fixed').getBoundingClientRect().bottom
+          };
           var group_id = text_bubbles[i].getAttribute('data-group');
           var group = document.getElementById('group' + (group_id || '1')); //if there is no group_id = 1
-          var bigbubble = group.querySelector('.bigbubble');
-          var smallbubble = document.createElement("div");
-
-          // console.log(position.left)
-          // console.log(position.top)
-
-          smallbubble.setAttribute("class", "smallbubble");
-          smallbubble.style.top =  position.top + 25.5 +'px'; //1.5rem (bubble diameter) - need to convert
-          smallbubble.style.left = position.left + 25.5 +'px';
-          group.appendChild(smallbubble)
-
-          var position2 = smallbubble.getBoundingClientRect();
-          // console.log(position2.left)
-          // console.log(position2.top)
-
-          //move bubbles to endpoint
-
-          var endpoint = group.querySelector('.endpoint')
-          var position3 = endpoint.getBoundingClientRect(); 
-
-          var centered = document.getElementById('page-centered');
-          var position4 = centered.getBoundingClientRect();
-
-          var centered = document.getElementById('fixed');
-          var position5 = centered.getBoundingClientRect();
-
-
-          var values = [
-            {x: 0, y: 0},
-            {x: position3.left - position4.left - 25.5 +'px', y: position3.top -position5.bottom + 'px' } //10rem bigbubble
-           ]
-
-          TweenMax.to(smallbubble, timeScale * 0.3, {
-          bezier: {values: values, type: "soft"},
-          onComplete: function () {
-            smallbubble.remove();
-            }
-          });
-
-        }
+          if (group_id != '3') {
+            animateBubble(startingPosition, finalPosition[group_id], group, "smallbubble");
+  
+          } else {
+            animateBubble(startingPosition, finalPosition[group_id], group, "smallbubble");
+            var group2 = document.getElementById('group2');
+            animateBubble(startingPosition, finalPosition['2'], group2, "smallbubble");
+          };
+        };
         text_bubbles[i].setAttribute("class", "text-bubble hidden");
       } else {
         text_bubbles[i].setAttribute("class", "text-bubble");
@@ -120,5 +127,3 @@ requestAnimationFrame(function() {
 
   }); 
 });
-
-//window.onresize()
